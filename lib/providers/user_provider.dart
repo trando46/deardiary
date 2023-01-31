815e84b846cde16b57fd3eb4ab3c36aaa827f92b
Henry Song <henry.song671@gmail.com>
2023-01-30 18:24:59 -0800

    added boilerplate template to user_provider.dart

---
+++ lib/providers/user_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:deardiary/models/user.dart';
+
+class UserProvider with ChangeNotifier {
+  UserModel? _user;
+
+  UserModel? get user => _user;
+
+  void setUser(UserModel user) {
+    _user = user;
+    notifyListeners();
+  }
+}
