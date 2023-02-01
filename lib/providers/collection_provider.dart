f3affb60bc2ee601ebddc29be749c90b0b1d1666
Henry Song <henry.song671@gmail.com>
2023-01-31 22:00:47 -0800

    added generic provider functions to collection_provider.dart

--- lib/providers/collection_provider.dart
+++ lib/providers/collection_provider.dart
@@ -1,3 +1,4 @@
+import 'package:deardiary/models/journalentry.dart';
 import 'package:flutter/material.dart';
 import 'package:deardiary/models/collection.dart';
 
@@ -8,6 +9,26 @@ class CollectionProvider with ChangeNotifier {
 
   void setCollection(CollectionModel collection) {
     _collection = collection;
+    notifyListeners();
+  }
+
+  void updateCollectionID(int collectionID) {
+    _collection!.collectionID = collectionID;
+    notifyListeners();
+  }
+
+  void updateOwnerID(int ownerID) {
+    _collection!.ownerID = ownerID;
+    notifyListeners();
+  }
+
+  void updateCollectionTitle(String collectionTitle) {
+    _collection!.collectionTitle = collectionTitle;
+    notifyListeners();
+  }
+
+  void updateCollectionEntries(List<JournalEntryModel> collectionEntries) {
+    _collection!.collectionEntries = collectionEntries;
     notifyListeners();
   }
 }
