baa179f5f27c42433c3103594b9433cebaf0919d
Henry Song <henry.song671@gmail.com>
2023-01-31 21:33:39 -0800

    added generic provider functions to journalentry_provider.dart

--- lib/providers/journalentry_provider.dart
+++ lib/providers/journalentry_provider.dart
@@ -10,4 +10,54 @@ class JournalEntryProvider with ChangeNotifier {
     _journalEntry = journalEntry;
     notifyListeners();
   }
+
+  void updateJournalEntryID(int journalEntryID) {
+    _journalEntry!.journalEntryID = journalEntryID;
+    notifyListeners();
+  }
+
+  void updateOwnerID(int ownerID) {
+    _journalEntry!.ownerID = ownerID;
+    notifyListeners();
+  }
+
+  void updateJournalEntryTitle(String journalEntryTitle) {
+    _journalEntry!.journalEntryTitle = journalEntryTitle;
+    notifyListeners();
+  }
+
+  void updateJournalEntryContent(String journalEntryContent) {
+    _journalEntry!.journalEntryContent = journalEntryContent;
+    notifyListeners();
+  }
+
+  void updateJournalEntryTags(List<String> journalEntryTags) {
+    _journalEntry!.journalEntryTags = journalEntryTags;
+    notifyListeners();
+  }
+
+  void updateJournalEntryImage(String journalEntryImage) {
+    _journalEntry!.journalEntryImage = journalEntryImage;
+    notifyListeners();
+  }
+
+  void updateJournalEntryGeo(String journalEntryGeo) {
+    _journalEntry!.journalEntryGeo = journalEntryGeo;
+    notifyListeners();
+  }
+
+  void updateJournalEntryCreationDate(DateTime journalEntryCreationDate) {
+    _journalEntry!.journalEntryCreationDate = journalEntryCreationDate;
+    notifyListeners();
+  }
+
+  void updateJournalEntryLastUpdate(DateTime journalEntryLastUpdate) {
+    _journalEntry!.journalEntryLastUpdate = journalEntryLastUpdate;
+    notifyListeners();
+  }
+
+  void deleteJournalEntry() {
+    _journalEntry = null;
+    notifyListeners();
+  }
 }
