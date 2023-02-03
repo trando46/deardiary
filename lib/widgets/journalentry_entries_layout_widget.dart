e8c1e2515bbb5e0ad0dfaed9f60a4e18cef39708
Henry Song <henry.song671@gmail.com>
2023-02-02 17:13:23 -0800

    corrected spacing in journalentry_entries_layout_widget.dart

--- lib/widgets/journalentry_entries_layout_widget.dart
+++ lib/widgets/journalentry_entries_layout_widget.dart
@@ -1,14 +1,10 @@
-
-
 import 'package:deardiary/providers/journalentry_provider.dart';
 import 'package:deardiary/widgets/journalentry_individual_ui_widget.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
-class JournalEntryEntriesLayoutWidget extends StatelessWidget{
-
+class JournalEntryEntriesLayoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<JournalEntryProvider>(context);
@@ -16,20 +12,20 @@ class JournalEntryEntriesLayoutWidget extends StatelessWidget{
     final entries = provider.journalEntry;
 
     // Need to take care of no todos and todos
-    return  ListView.separated(
+    return ListView.separated(
       // Special effect of scrolling and display the task cards
-      itemCount: entries.legnth,
+      itemCount: entries.length,
       scrollDirection: Axis.vertical,
       dragStartBehavior: DragStartBehavior.start,
       // Adding the spacing between each task using the container
       separatorBuilder: (context, index) => Container(
         height: 18,
-        color: Colors.black12,),
+        color: Colors.black12,
+      ),
       itemBuilder: (context, index) {
         final entry = entries[index];
         return JournalEntryIndividualUIWidget(journalEntryModel: entry);
       },
     );
   }
-
 }
