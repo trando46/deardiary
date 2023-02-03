01b7882e18e5507469b81a9981887c3f216ecbae
Henry Song <henry.song671@gmail.com>
2023-02-03 12:12:47 -0800

    automatic space fixing

--- lib/widgets/journalentry_individual_ui_widget.dart
+++ lib/widgets/journalentry_individual_ui_widget.dart
@@ -1,13 +1,10 @@
-
 import 'package:deardiary/models/journalentry.dart';
 import 'package:deardiary/providers/journalentry_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 
-
 class JournalEntryIndividualUIWidget extends StatelessWidget {
-
   // Declared the variables
   final JournalEntryModel journalEntryModel;
 
@@ -17,71 +14,65 @@ class JournalEntryIndividualUIWidget extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) =>
-      ClipPath(
-
+  Widget build(BuildContext context) => ClipPath(
           child: Slidable(
-            key: Key(journalEntryModel.journalEntryID),
-            //This is for selecting the task card it will bring you the the task
-            // card containing the information
-            child: individualUITaskCardWidget(context),
+        key: Key(journalEntryModel.journalEntryID),
 
-            // The start action pane is the one at the left or the top side. This
-            // will be the edit and remove functionality
-            startActionPane: ActionPane(
-              key: ValueKey('start'),
+        // The start action pane is the one at the left or the top side. This
+        // will be the edit and remove functionality
+        startActionPane: ActionPane(
+          key: ValueKey('start'),
 
-              // A motion is a widget used to control how the pane animates.
-              motion: const ScrollMotion(),
+          // A motion is a widget used to control how the pane animates.
+          motion: const ScrollMotion(),
 
-              // A pane can dismiss the Slidable.
-              dismissible: DismissiblePane(onDismissed: () {}),
+          // A pane can dismiss the Slidable.
+          dismissible: DismissiblePane(onDismissed: () {}),
 
-              // All actions are defined in the children parameter.
-              children: [
-                // Allowing the user to edit
-                SlidableAction(
-                  onPressed: (_) {
-                    //TODO: route it to the appropriate place
-                    //editTodo(context, journalEntryModel);
-                  },
-                  backgroundColor: Colors.black38,
-                  foregroundColor: Colors.white,
-                  icon: Icons.edit,
-                  label: 'Edit',
-                ),
-
-                // Allowing the user to delete
-                SlidableAction(
-                  onPressed: (_) {
-                    //TODO: route it to the appropriate place
-                    //deleteTodo(context, journalEntryModel);
-                  },
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.white,
-                  icon: Icons.delete,
-                  label: 'Delete',
-                )
-              ],
+          // All actions are defined in the children parameter.
+          children: [
+            // Allowing the user to edit
+            SlidableAction(
+              onPressed: (_) {
+                //TODO: route it to the appropriate place
+                //editTodo(context, journalEntryModel);
+              },
+              backgroundColor: Colors.black38,
+              foregroundColor: Colors.white,
+              icon: Icons.edit,
+              label: 'Edit',
             ),
-          )
-      );
 
+            // Allowing the user to delete
+            SlidableAction(
+              onPressed: (_) {
+                //TODO: route it to the appropriate place
+                //deleteTodo(context, journalEntryModel);
+              },
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+            )
+          ],
+        ),
+        //This is for selecting the task card it will bring you the the task
+        // card containing the information
+        child: individualUITaskCardWidget(context),
+      ));
 
-  /***
-   * This is for the ui design of the task card. Only display the title of the
-   * card
-   */
-  Widget individualUITaskCardWidget(BuildContext context) =>
-      GestureDetector(
+  /// *
+  /// This is for the ui design of the task card. Only display the title of the
+  /// card
+  Widget individualUITaskCardWidget(BuildContext context) => GestureDetector(
         // when tap on the card it will display the todos content
-        key: ValueKey('gesture_detector'),
+        key: const ValueKey('gesture_detector'),
         //onTap: () => displayTodoContent(context, journalEntryModel),
-        onTap: (){}, //TODO: route to the appropriate page later
+        onTap: () {}, //TODO: route to the appropriate page later
         child: Container(
           color: Colors.white,
           // Padding between the interior boxes
-          padding: EdgeInsets.all(15),
+          padding: const EdgeInsets.all(15),
           child: Row(
             children: [
               // This is a place holder for when we need to add more attributes
@@ -93,9 +84,7 @@ class JournalEntryIndividualUIWidget extends StatelessWidget {
                     journalEntryModel.journalEntryTitle,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      color: Theme
-                          .of(context)
-                          .primaryColorDark,
+                      color: Theme.of(context).primaryColorDark,
                       fontSize: 20,
                     ),
                   ),
