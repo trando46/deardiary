2c137e91c98b8ce8b2be079a8f975bdc54e15c6a
greg reneris <greg@reneris.com>
2023-02-12 22:54:11 -0800

    fixed optional parts in an entry model when copying.

--- lib/models/journalentry.dart
+++ lib/models/journalentry.dart
@@ -2,15 +2,15 @@
 final String tableJournalEntry = 'journalentry';
 
 class JournalEntryModelFields{
-  static final String id = '_id';
-  static final String ownerId = '_oid';
-  static final String journalEntryTitle = 'journalEntryTitle';
-  static final String journalEntryContent = 'journalEntryContent';
-  static final String journalEntryTags = "journalEntryTags"; // will parse a lsit of entry tags later.
-  static final String journalEntryImage = "journalEntryImage";
+  static const String id = '_id';
+  static const String ownerID = '_oid';
+  static const String journalEntryTitle = 'journalEntryTitle';
+  static const String journalEntryContent = 'journalEntryContent';
+  static const String journalEntryTags = "journalEntryTags"; // will parse a lsit of entry tags later.
+  static const String journalEntryImage = "journalEntryImage";
   static const String journalEntryGeo = "journalEntryGeo";
-  static final String journalEntryCreationDate = "creationTime";         //DateTime.now();
-  static final String journalEntryLastUpdate = "updateTime";
+  static const String journalEntryCreationDate = "creationTime";         //DateTime.now();
+  static const String journalEntryLastUpdate = "updateTime";
 
 
 }
@@ -40,9 +40,14 @@ class JournalEntryModel{
 
   });
 
+  //Needed for the database create as it couldn't see ownerID.
+  String returnOwnerID(){
+    return ownerID;
+  }
+
   Map<String, Object?> toJson() => {
     JournalEntryModelFields.id: journalEntryID,
-    JournalEntryModelFields.ownerId: ownerID,
+    JournalEntryModelFields.ownerID: ownerID,
     JournalEntryModelFields.journalEntryTitle: journalEntryTitle,
     JournalEntryModelFields.journalEntryContent: journalEntryContent,
     JournalEntryModelFields.journalEntryGeo: journalEntryGeo,
@@ -51,5 +56,31 @@ class JournalEntryModel{
     JournalEntryModelFields.journalEntryCreationDate: journalEntryCreationDate.toIso8601String(), // converting
     JournalEntryModelFields.journalEntryLastUpdate: journalEntryLastUpdate.toIso8601String() // converting
   };
+
+  JournalEntryModel copy({
+  String? journalEntryID,
+  String? ownerID,
+  String? journalEntryTitle,
+  String? journalEntryContent,
+  List<String>? journalEntryTags,
+  String? journalEntryImage,
+  String? journalEntryGeo,
+  DateTime? journalEntryCreationDate,
+  DateTime? journalEntryLastUpdate,
+
+}) => JournalEntryModel(
+    journalEntryID : journalEntryID ?? this.journalEntryID,
+    ownerID : ownerID ?? this.ownerID,
+    journalEntryTitle : journalEntryTitle ?? this.journalEntryTitle ,
+    journalEntryContent : journalEntryContent ?? this.journalEntryContent ,
+    //journalEntryTags : journalEntryTags ?? this.journalEntryTags, //not part of model?
+    journalEntryImage : journalEntryImage ?? this.journalEntryImage,
+    journalEntryGeo : journalEntryGeo ?? this.journalEntryGeo,
+    journalEntryCreationDate : journalEntryCreationDate ?? this.journalEntryCreationDate,
+    journalEntryLastUpdate : journalEntryLastUpdate ?? this.journalEntryLastUpdate,
+  );
+
+
+
 
 }
