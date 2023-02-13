138460801dc719111cc2bf2bfaad0916330d7cc0
greg reneris <greg@reneris.com>
2023-02-13 02:21:50 -0800

    added const to layout widget constructor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final tabs = [
-      JournalEntryEntriesLayoutWidget(),
+      const JournalEntryEntriesLayoutWidget(),
       // Settings(),
     ];
 
