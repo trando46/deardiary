e8f9e654355c047b50e66016f6e03218ed850673
greg reneris <greg@reneris.com>
2023-02-13 02:21:05 -0800

    Adjusted db code to more effectively create and get all entries.

--- lib/database/diary_database.dart
+++ lib/database/diary_database.dart
@@ -4,21 +4,24 @@ import 'package:deardiary/models/journalentry.dart';
 //import 'package:deardiary/models/user.dart';
 
 
+/**
+ * DiaryDatabase is the local Sqlite.
+ */
 class DiaryDatabase{
 
-  static final DiaryDatabase instance = DiaryDatabase.init();
+  static final DiaryDatabase instance = DiaryDatabase._init();
 
   static Database? _database; //creating db.
 
-  DiaryDatabase.init(); //initializing db.
+  DiaryDatabase._init(); //initializing db.
 
 
   Future<Database> get database async{
-    if(_database != null) return database; //return database if it does not exist.
+    if(_database != null) return _database!; //return database if it does not exist.
 
     //otherwise create new db.
     _database = await _initDB('deardiary.db');
-    return _database!; //! is
+    return _database!; //null safety for database.
   }
 
   //manage path
@@ -27,8 +30,14 @@ class DiaryDatabase{
     //final path = join(dbPath, filePath);
     final path = dbPath+filePath;
 
-    return await openDatabase(path, version: 1, onCreate: _createDB);
+    //return await openDatabase(path, version: 3, onCreate: _createDB, onUpgrade: _upgradeDB);
+    return await openDatabase(path, version: 2, onCreate: _createDB);
   }
+
+
+/*  Future _upgradeDB(Database db, int oldversion, int newversion) async{
+    return _createDB(db, newversion);
+  }*/
 
   Future _createDB(Database db, int version) async{
     //placeholder for schema
@@ -36,30 +45,62 @@ class DiaryDatabase{
     const integerType = "INTEGER NOT NULL";
     const textType = "TEXT NOT NULL";
 
-
+    //table matching our journal entry model.
     await db.execute('''
         CREATE TABLE $tableJournalEntry (
-        ${JournalEntryModelFields.id} $idType,
+        ${JournalEntryModelFields.journalEntryID} $idType,
         ${JournalEntryModelFields.ownerID} $integerType,
         ${JournalEntryModelFields.journalEntryTitle} $textType,
         ${JournalEntryModelFields.journalEntryContent} $textType,
         ${JournalEntryModelFields.journalEntryTags} $textType,
         ${JournalEntryModelFields.journalEntryImage} $textType,
-        ${JournalEntryModelFields.journalEntryGeo} $textType, //Switch to better? geo location later
-        ${JournalEntryModelFields.journalEntryCreationDate} $textType, //Convert out on parse
-        ${JournalEntryModelFields.journalEntryLastUpdate} $textType, //Convert back out on parse
+        ${JournalEntryModelFields.journalEntryGeo} $textType,
+        ${JournalEntryModelFields.journalEntryCreationDate} $textType,
+        ${JournalEntryModelFields.journalEntryLastUpdate} $textType
     )
     ''');
   }
 
-  Future<JournalEntryModel> create(JournalEntryModel journal) async {
+  Future<JournalEntryModel> read(int id) async {
+    // Get a reference to the database.
     final db = await instance.database;
 
-    final id = await db.insert(tableJournalEntry, journal.toJson());
+    // Query the table for all The Dogs.
+    final maps = await db.query(
+        tableJournalEntry,
+        columns: JournalEntryModelFields.values,
+        where: '${JournalEntryModelFields.journalEntryID} = ?',
+        whereArgs: [id],
+    );
 
-    return journal.copy(journalEntryID: id.toString());
+    if (maps.isEmpty)
+    {
+      throw Exception("Journal Entry Missing ID:  $id");
+    }
+
+    return JournalEntryModel.fromJson(maps.first);
 
   }
+
+  Future<List<JournalEntryModel>> getAll() async {
+    final db = await instance.database;
+
+    final allEntries = await db.query(tableJournalEntry);
+
+    return allEntries.map((j) => JournalEntryModel.fromJson(j)).toList();
+
+
+  }
+
+
+  Future create(JournalEntryModel journal) async {
+    final db = await instance.database;
+
+    await db.insert(tableJournalEntry, journal.toJson());
+
+  }
+
+
 
   Future close() async{
     final db = await instance.database;
