c0b3b1a2b75b525f00b3ea6ab356ec27735691a4
greg reneris <greg@reneris.com>
2023-03-07 21:48:22 -0800

    Added in firebase to the app. Anon auth at the moment but we connected.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,35 @@
 import 'package:deardiary/pages/home_page.dart';
 import 'package:deardiary/providers/journalentry_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  //used for testing purposes.
+  //await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
+  try {
+    final userCredential =
+        await FirebaseAuth.instance.signInAnonymously();
+    print("Signed in with temporary account.");
+  } on FirebaseAuthException catch (e) {
+    switch (e.code) {
+      case "operation-not-allowed":
+        print("Anonymous auth hasn't been enabled for this project.");
+        break;
+      default:
+        print("Unknown error.");
+    }
+  }
+
   runApp(MyApp());
 }
 
