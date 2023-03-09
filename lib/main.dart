071160255556ff85d79626a7449ad3c0a3b4f2f9
Henry Song <henry.song671@gmail.com>
2023-03-08 23:48:38 -0800

    commented out anonymous authentication in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:deardiary/pages/home_page.dart';
+import 'package:deardiary/pages/landing_page.dart';
+import 'package:deardiary/pages/authentication_page.dart';
 import 'package:deardiary/providers/journalentry_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +9,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -16,42 +17,37 @@ void main() async {
   //used for testing purposes.
   //await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
-  try {
-    final userCredential =
-        await FirebaseAuth.instance.signInAnonymously();
-    print("Signed in with temporary account.");
-  } on FirebaseAuthException catch (e) {
-    switch (e.code) {
-      case "operation-not-allowed":
-        print("Anonymous auth hasn't been enabled for this project.");
-        break;
-      default:
-        print("Unknown error.");
-    }
-  }
+  // try {
+  //   final userCredential = await FirebaseAuth.instance.signInAnonymously();
+  //   print("Signed in with temporary account.");
+  // } on FirebaseAuthException catch (e) {
+  //   switch (e.code) {
+  //     case "operation-not-allowed":
+  //       print("Anonymous auth hasn't been enabled for this project.");
+  //       break;
+  //     default:
+  //       print("Unknown error.");
+  //   }
+  // }
 
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   static final String title = 'Dear Diary';
 
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
-    create: (context) => JournalEntryProvider(),
-    child: MaterialApp(
-      home: HomePage(),
-      title: title,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        primarySwatch: Colors.blueGrey,
-        scaffoldBackgroundColor: Colors.blueGrey.shade200,
-      ),
-    ),
-  );
+        create: (context) => JournalEntryProvider(),
+        child: MaterialApp(
+          home: LandingPage(),
+          title: title,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: Colors.black,
+            primarySwatch: Colors.blueGrey,
+            scaffoldBackgroundColor: Colors.blueGrey.shade200,
+          ),
+        ),
+      );
 }
-
-
-
