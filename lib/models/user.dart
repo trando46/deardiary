b87ea5911f5b8be1eb6735ac78d1ba996c2cf4ff
Henry Song <henry.song671@gmail.com>
2023-03-09 17:57:49 -0800

    modified userModel to accomodate users

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,34 +1,45 @@
 final String tableUserModel = 'usermodel';
 
-
-class UserModelFields{
+class UserModelFields {
   static final String userID = '_id';
   static final String userName = 'userName';
-  static final String userDob = '0';
-  static final String userDescription = 'userDescription';
-  static final String userPicture = 'userPicture';
+  static final String userDob = 'userDob';
+  //static final String userDescription = 'userDescription';
+  static final String email = 'email';
+  //static final String userPicture = 'userPicture';
 }
 
-class UserModel  {
-  int userID;
+class UserModel {
+  String userID;
   String userName;
-  int userDob;
-  String userDescription;
-  String userPicture;
+  String userDob;
+  //String userDescription;
+  String email;
+  //String userPicture;
 
   UserModel({
     required this.userID,
     required this.userName,
-    this.userDob = 0,
-    this.userDescription = '',
-    this.userPicture ='',
-});
+    required this.userDob,
+    //this.userDescription = '',
+    required this.email,
+    //this.userPicture = '',
+  });
 
+  UserModel.fromJson(Map<String, dynamic> json)
+      : userID = json[UserModelFields.userID],
+        userName = json[UserModelFields.userName],
+        userDob = json[UserModelFields.userDob],
+        //userDescription = json[UserModelFields.userDescription],
+        email = json[UserModelFields.email];
+  //userPicture = json[UserModelFields.userPicture];
 
-
-
-
-
-
-
+  Map<String, dynamic> toJson() => {
+        UserModelFields.userID: userID,
+        UserModelFields.userName: userName,
+        UserModelFields.userDob: userDob,
+        //UserModelFields.userDescription: userDescription,
+        UserModelFields.email: email,
+        //UserModelFields.userPicture: userPicture,
+      };
 }
