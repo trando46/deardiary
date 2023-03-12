497af65bea84a73e86a53f8ddc9b6d3c50b6ef7e
greg reneris <greg@reneris.com>
2023-03-11 16:17:45 -0800

    Adjusting for creation management.

--- lib/database/firestore_methods.dart
+++ lib/database/firestore_methods.dart
@@ -8,20 +8,16 @@ import '../models/journalentry.dart';
 
 class FireStoreMethods {
 
-  var onlineDB = FirebaseFirestore.instance;
-
-
   //TODO: Add method to add journal entry to user collection of journalentries.
   Future<void> updateOrAddJournal(JournalEntryModel journal)
   async {
 
-      var defaultDateTime = DateTime.now();
-      String defaultDateTimeString = defaultDateTime.toIso8601String();
-      // var defaultRelationship = new TaskRelationship(dstTaskID: "1", relation: TaskRelationshipEnum.subtaskOf);
-      // var jsonRelationship = defaultRelationship.toJson();
-      var db = FirebaseFirestore.instance;
+    var defaultDateTime = DateTime.now();
+    String defaultDateTimeString = defaultDateTime.toIso8601String();
+    var db = FirebaseFirestore.instance;
 
-      /*
+/*
+
       int? journalEntryID;
       String ownerID;
       String journalEntryTitle;
@@ -32,25 +28,26 @@ class FireStoreMethods {
       String journalEntryGeo;
       DateTime journalEntryCreationDate;
       DateTime journalEntryLastUpdate;
-      */
+*/
 
-      final outgoingJournal = <String, String>{
-        "ownerID": journal.ownerID,
-        "journalEntryTitle": journal.journalEntryTitle,
-        "journalEntryContent": journal.journalEntryContent,
-        "journalEntryImage": journal.journalEntryImage,
-        "journalEntryGeo": journal.journalEntryGeo, //TODO: Parse this maybe?
-        "journalEntryCreationDate" : journal.journalEntryCreationDate.toIso8601String(),
-        "journalEntryLastUpdate" : journal.journalEntryLastUpdate.toIso8601String(),
-      };
 
-      //var doc = db.collection("users").doc(journal.journalEntryID!.toString()); //use doc id to update. Not sure if this is the right one to use.
-      var doc = db.collection("users").doc(journal.ownerID); //use doc id to update. Not sure if this is the right one to use.
-      var doc2 = doc.collection("journals").doc(journal.journalEntryID!.toString());
-      doc2.set(outgoingJournal)
-          //.then((value) => _getAllEntriesAsync(),
-          .then((value) => null,
-          onError: (e) => print("Error writing document: $e"));
+    final outgoingJournal = <String, String>{
+      "ownerID": journal.ownerID,
+      "journalEntryTitle": journal.journalEntryTitle,
+      "journalEntryContent": journal.journalEntryContent,
+      "journalEntryImage": journal.journalEntryImage,
+      "journalEntryGeo": journal.journalEntryGeo, //TODO: Parse this maybe?
+      "journalEntryCreationDate" : journal.journalEntryCreationDate.toIso8601String(),
+      "journalEntryLastUpdate" : journal.journalEntryLastUpdate.toIso8601String(),
+    };
+
+    //var doc = db.collection("users").doc(journal.journalEntryID!.toString()); //use doc id to update. Not sure if this is the right one to use.
+    var doc = db.collection("users").doc(journal.ownerID); //use doc id to update. Not sure if this is the right one to use.
+    var doc2 = doc.collection("journals").doc();
+    doc2.set(outgoingJournal)
+    //.then((value) => _getAllEntriesAsync(),
+        .then((value) => null,
+        onError: (e) => print("Error writing document: $e"));
 
   }
 
