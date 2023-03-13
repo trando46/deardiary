fd0807d70e1457002c16dda57199faedf9606c00
Henry Song <henry.song671@gmail.com>
2023-03-13 15:55:28 -0700

    added update image function to firebase_provider

--- lib/providers/firestore_provider.dart
+++ lib/providers/firestore_provider.dart
@@ -1,24 +1,18 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../models/journalentry.dart';
 
-class FirestoreProvider
-{
+class FirestoreProvider {
   final FirebaseFirestore firestore;
-  final FirebaseAuth      fireauth;
+  final FirebaseAuth fireauth;
 
   FirestoreProvider([FirebaseFirestore? firestore, FirebaseAuth? fireauth])
-    : this.firestore = firestore ?? FirebaseFirestore.instance
-    , this.fireauth = fireauth ?? FirebaseAuth.instance;
-
+      : this.firestore = firestore ?? FirebaseFirestore.instance,
+        this.fireauth = fireauth ?? FirebaseAuth.instance;
 
   //Add method to add journal entry to user collection of journalentries.
-  Future<void> addJournal(JournalEntryModel journal)
-  async {
-
+  Future<void> addJournal(JournalEntryModel journal) async {
     var defaultDateTime = DateTime.now();
     String defaultDateTimeString = defaultDateTime.toIso8601String();
     //var db = _filestore;
@@ -29,50 +23,49 @@ class FirestoreProvider
       "journalEntryContent": journal.journalEntryContent,
       "journalEntryImage": journal.journalEntryImage,
       "journalEntryGeo": journal.journalEntryGeo, //TODO: Parse this maybe?
-      "journalEntryCreationDate" : journal.journalEntryCreationDate.toIso8601String(),
-      "journalEntryLastUpdate" : journal.journalEntryLastUpdate.toIso8601String(),
+      "journalEntryCreationDate":
+          journal.journalEntryCreationDate.toIso8601String(),
+      "journalEntryLastUpdate":
+          journal.journalEntryLastUpdate.toIso8601String(),
     };
 
     //var doc = db.collection("users").doc(journal.journalEntryID!.toString()); //use doc id to update. Not sure if this is the right one to use.
-    var doc = firestore.collection("users").doc(journal.ownerID); //use doc id to update. Not sure if this is the right one to use.
+    var doc = firestore.collection("users").doc(journal
+        .ownerID); //use doc id to update. Not sure if this is the right one to use.
     var doc2 = doc.collection("journals").doc();
-    doc2.set(outgoingJournal)
-    //.then((value) => _getAllEntriesAsync(),
+    doc2
+        .set(outgoingJournal)
+        //.then((value) => _getAllEntriesAsync(),
         .then((value) => null,
-        onError: (e) => print("Error writing document: $e"));
-
+            onError: (e) => print("Error writing document: $e"));
   }
 
   ///Retrieves all online journal entries for a particular user.
-  Future<List<JournalEntryModel>> getAllOnlineEntries()
-  async {
-
+  Future<List<JournalEntryModel>> getAllOnlineEntries() async {
     //var db = FirebaseFirestore.instance;
     //var user = FirebaseAuth.instance.currentUser;
-    List<JournalEntryModel> returnList = <JournalEntryModel>[]; //required literal <>[] assignment
+    List<JournalEntryModel> returnList =
+        <JournalEntryModel>[]; //required literal <>[] assignment
 
     var snapshot = firestore.collection("users").doc(fireauth.currentUser!.uid);
     var snapshot2 = await snapshot.collection("journals").get();
 
     JournalEntryModel newJournalEntry;
 
-    for (var doc in snapshot2.docs)
-    {
+    for (var doc in snapshot2.docs) {
       print(doc.id);
       var data = doc.data;
       newJournalEntry = convertJournalEntry(doc, fireauth.currentUser!.uid);
       returnList.add(newJournalEntry);
-
     }
     //var x = 2;
 
     return returnList;
   }
 
-
   ///Conversion method for journal entries retrieved from firestore.
-  JournalEntryModel convertJournalEntry(QueryDocumentSnapshot doc, String userID)
-  {
+  JournalEntryModel convertJournalEntry(
+      QueryDocumentSnapshot doc, String userID) {
     return JournalEntryModel(
       ownerID: userID,
       onlineID: doc.id,
@@ -84,9 +77,7 @@ class FirestoreProvider
       journalEntryLastUpdate: DateTime.parse(doc['journalEntryLastUpdate']),
       //journalEntryID: ,
     );
-
   }
-
 
   Future<void> updateJournal(JournalEntryModel journal) async {
     var doc = firestore
@@ -98,10 +89,8 @@ class FirestoreProvider
     doc.update({
       "journalEntryTitle": journal.journalEntryTitle,
       "journalEntryContent": journal.journalEntryContent,
-      "journalEntryImage": journal.journalEntryImage,
-      "journalEntryGeo": journal.journalEntryGeo,
       "journalEntryLastUpdate":
-      journal.journalEntryLastUpdate.toIso8601String(),
+          journal.journalEntryLastUpdate.toIso8601String(),
     });
   }
 
@@ -115,7 +104,21 @@ class FirestoreProvider
     doc.update({
       "journalEntryGeo": journal.journalEntryGeo,
       "journalEntryLastUpdate":
-      journal.journalEntryLastUpdate.toIso8601String(),
+          journal.journalEntryLastUpdate.toIso8601String(),
+    });
+  }
+
+  Future<void> updateImage(JournalEntryModel journal) async {
+    var doc = firestore
+        .collection("users")
+        .doc(journal.ownerID)
+        .collection("journals")
+        .doc(journal.onlineID);
+
+    doc.update({
+      "journalEntryImage": journal.journalEntryImage,
+      "journalEntryLastUpdate":
+          journal.journalEntryLastUpdate.toIso8601String(),
     });
   }
 
@@ -129,6 +132,4 @@ class FirestoreProvider
 
     doc.delete();
   }
-
 }
-
