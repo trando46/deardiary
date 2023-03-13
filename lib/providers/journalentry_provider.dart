070f831f29e5a760ece003aeffed03792ba7a6c9
Henry Song <henry.song671@gmail.com>
2023-03-12 22:36:29 -0700

    added update and delete to both firestore and local db

--- lib/providers/journalentry_provider.dart
+++ lib/providers/journalentry_provider.dart
@@ -5,7 +5,6 @@ import 'package:deardiary/models/journalentry.dart';
 import 'package:deardiary/database/firestore_methods.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 class JournalEntryProvider with ChangeNotifier {
   List<JournalEntryModel> _journalEntry = [];
   FireStoreMethods fsm = FireStoreMethods();
@@ -20,7 +19,6 @@ class JournalEntryProvider with ChangeNotifier {
   Future _getAllEntriesAsync() async {
     //final entries = await DiaryDatabase.instance.getAll();
     //_journalEntry = entries;
-
 
     _journalEntry = await fsm.getAllOnlineEntries();
 
@@ -41,41 +39,54 @@ class JournalEntryProvider with ChangeNotifier {
 
     //TODO: Also add entry to users collection of journals.
 
-
     _getAllEntriesAsync();
     //notifyListeners();
   }
 
-  void updateOwnerID(JournalEntryModel entry, int ownerID) {
-    if (_journalEntry.contains(entry)) {
-      _journalEntry[_journalEntry.indexOf(entry)].ownerID = ownerID.toString();
-      _journalEntry[_journalEntry.indexOf(entry)].journalEntryLastUpdate =
-          DateTime.now();
-    }
-    notifyListeners();
+  void updateJournalEntry(JournalEntryModel journalEntry, String title,
+      String content, String geo, String image) {
+    journalEntry.journalEntryTitle = title;
+    journalEntry.journalEntryContent = content;
+    journalEntry.journalEntryGeo = geo;
+    journalEntry.journalEntryImage = image;
+    journalEntry.journalEntryLastUpdate = DateTime.now();
+
+    DiaryDatabase.instance.update(journalEntry);
+    fsm.updateJournal(journalEntry);
+
+    _getAllEntriesAsync();
   }
 
-  void updateJournalEntryTitle(
-      JournalEntryModel entry, String journalEntryTitle) {
-    if (_journalEntry.contains(entry)) {
-      _journalEntry[_journalEntry.indexOf(entry)].journalEntryTitle =
-          journalEntryTitle;
-      _journalEntry[_journalEntry.indexOf(entry)].journalEntryLastUpdate =
-          DateTime.now();
-    }
-    notifyListeners();
-  }
+  // void updateOwnerID(JournalEntryModel entry, int ownerID) {
+  //   if (_journalEntry.contains(entry)) {
+  //     _journalEntry[_journalEntry.indexOf(entry)].ownerID = ownerID.toString();
+  //     _journalEntry[_journalEntry.indexOf(entry)].journalEntryLastUpdate =
+  //         DateTime.now();
+  //   }
+  //   notifyListeners();
+  // }
 
-  void updateJournalEntryContent(
-      JournalEntryModel entry, String journalEntryContent) {
-    if (_journalEntry.contains(entry)) {
-      _journalEntry[_journalEntry.indexOf(entry)].journalEntryContent =
-          journalEntryContent;
-      _journalEntry[_journalEntry.indexOf(entry)].journalEntryLastUpdate =
-          DateTime.now();
-    }
-    notifyListeners();
-  }
+  // void updateJournalEntryTitle(
+  //     JournalEntryModel entry, String journalEntryTitle) {
+  //   if (_journalEntry.contains(entry)) {
+  //     _journalEntry[_journalEntry.indexOf(entry)].journalEntryTitle =
+  //         journalEntryTitle;
+  //     _journalEntry[_journalEntry.indexOf(entry)].journalEntryLastUpdate =
+  //         DateTime.now();
+  //   }
+  //   notifyListeners();
+  // }
+
+  // void updateJournalEntryContent(
+  //     JournalEntryModel entry, String journalEntryContent) {
+  //   if (_journalEntry.contains(entry)) {
+  //     _journalEntry[_journalEntry.indexOf(entry)].journalEntryContent =
+  //         journalEntryContent;
+  //     _journalEntry[_journalEntry.indexOf(entry)].journalEntryLastUpdate =
+  //         DateTime.now();
+  //   }
+  //   notifyListeners();
+  // }
 
   void updateJournalEntryTags(
       JournalEntryModel entry, List<String> journalEntryTags) {
@@ -100,18 +111,19 @@ class JournalEntryProvider with ChangeNotifier {
   }
 
   void updateJournalEntryGeo(JournalEntryModel entry, String journalEntryGeo) {
-    if (_journalEntry.contains(entry)) {
-      _journalEntry[_journalEntry.indexOf(entry)].journalEntryGeo =
-          journalEntryGeo;
-      _journalEntry[_journalEntry.indexOf(entry)].journalEntryLastUpdate =
-          DateTime.now();
-    }
-    notifyListeners();
+    entry.journalEntryGeo = journalEntryGeo;
+    entry.journalEntryLastUpdate = DateTime.now();
+
+    DiaryDatabase.instance.update(entry);
+    fsm.updateGeolocation(entry);
+
+    _getAllEntriesAsync();
   }
 
   void deleteJournalEntry(JournalEntryModel entry) {
     _journalEntry.remove(entry);
     DiaryDatabase.instance.delete(entry);
+    fsm.deleteJournal(entry);
     _getAllEntriesAsync();
 
     notifyListeners();
