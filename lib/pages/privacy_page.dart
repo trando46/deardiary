19385ea14498557858b4708cdbeeafa6a9dfc826
baotran <bdo@seattleu.edu>
2023-03-13 08:26:24 -0700

    adding keys to the privacy_page.dart

--- lib/pages/privacy_page.dart
+++ lib/pages/privacy_page.dart
@@ -14,6 +14,7 @@ class PrivacyPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      key: Key("PrivacyPage"),
       appBar: AppBar(
         title: const Text('Privacy'),
       ),
