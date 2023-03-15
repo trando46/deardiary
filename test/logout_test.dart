27072b70dbbf7eb25a3ee19ca04c4e5866e65947
greg reneris <greg@reneris.com>
2023-03-14 21:31:13 -0700

    Adding logout tests

---
+++ test/logout_test.dart
@@ -0,0 +1,90 @@
+//import 'package:flutter/material.dart';
+import 'package:deardiary/pages/authentication_page.dart';
+import 'package:deardiary/widgets/journalentry_display_entry_widget.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:deardiary/main.dart';
+import 'package:deardiary/providers/firestore_provider.dart';
+
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+// https://pub.dev/packages/firebase_auth_mocks
+
+import 'package:deardiary/pages/home_page.dart';
+
+
+
+
+void main() async
+{
+  final mockUser = MockUser(isAnonymous: false,
+      uid: 'mockid',
+      email: 'mock@mock.com',
+      displayName: 'mocky');
+  final auth = MockFirebaseAuth(mockUser: mockUser, signedIn: true);
+  final fsp = FirestoreProvider(FakeFirebaseFirestore(), auth);
+
+
+  testWidgets(
+      "Find the dropdown button and go to logout button and press cancel will return user to home page", (WidgetTester tester) async {
+    // Create a model
+    await tester.pumpWidget((MyApp(fsp)));
+
+    // expect
+    expect(find.byType(HomePage), findsOneWidget);
+    // expect(find.byType(PopupMenuButton), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsOneWidget);
+    var menuOption = find.byKey(const Key("HomePage popupMenu button"));
+    expect(menuOption, findsOneWidget);
+
+    await tester.tap(menuOption);
+    await tester.pumpAndSettle();
+    expect(find.text("Logout"), findsOneWidget);
+
+    await tester.tap(find.text("Logout"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("Cancel"), findsOneWidget);
+
+    await tester.tap(find.text("Cancel"));
+    await tester.pumpAndSettle();
+    expect(find.byType(HomePage), findsOneWidget);
+
+  }
+  );
+
+  testWidgets(
+      "Find the dropdown button and go to logout button and press sign out will return user to auth page", (WidgetTester tester) async {
+    // Create a model
+    await tester.pumpWidget((MyApp(fsp)));
+
+    // expect
+    expect(find.byType(HomePage), findsOneWidget);
+    // expect(find.byType(PopupMenuButton), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsOneWidget);
+    var menuOption = find.byKey(const Key("HomePage popupMenu button"));
+    expect(menuOption, findsOneWidget);
+
+    await tester.tap(menuOption);
+    await tester.pumpAndSettle();
+    expect(find.text("Logout"), findsOneWidget);
+
+    await tester.tap(find.text("Logout"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("Sign Out"), findsAtLeastNWidgets(2));
+
+    await tester.tap(find.text("Sign Out").last);
+    await tester.pumpAndSettle();
+
+    //var loginkey = find.byKey(const Key('login'));
+
+    //expect(find.byKey(const Key('login')), findsOneWidget);
+
+  }
+  );
+
+
+}
